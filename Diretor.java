fad9bbb2b58f0c1934f280882b18caa4248d33d9
Clarice Maria Souza Copstein <121323583+Clacops@users.noreply.github.com>
2023-12-19 15:53:42 -0300

    Update Diretor.java
    cfe combinado

--- Diretor.java
+++ Diretor.java
@@ -1,6 +1,9 @@
-public class Diretor extends Ator {
+
+public class Diretor extends ProfissionaisCinema {
 
     public Diretor(String nome, String nacionalidade, String premiacoes) {
+
+        // inicializa variáveis de instância
         super(nome, nacionalidade, premiacoes);
     }
 }
