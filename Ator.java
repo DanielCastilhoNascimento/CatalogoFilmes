020cc73f9867277cc837639861181c3b878fc30b
Clarice Maria Souza Copstein <121323583+Clacops@users.noreply.github.com>
2023-12-19 15:53:03 -0300

    Update Ator.java
    cfe combinado

--- Ator.java
+++ Ator.java
@@ -1,35 +1,6 @@
-public class Ator {
-    private String nome;
-    private String nacionalidade;
-    private String premiacoes;
-
+public class Ator extends ProfissionaisCinema{
     public Ator(String nome, String nacionalidade, String premiacoes) {
-        this.nome = nome;
-        this.nacionalidade = nacionalidade;
-        this.premiacoes = premiacoes;
-    }
 
-    public String getNome() {
-        return nome;
+        // inicializa variáveis de instância
+        super(nome, nacionalidade, premiacoes);
     }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNacionalidade() {
-        return nacionalidade;
-    }
-
-    public void setNacionalidade(String nacionalidade) {
-        this.nacionalidade = nacionalidade;
-    }
-
-    public String getPremiacoes() {
-        return premiacoes;
-    }
-
-    public void setPremiacoes(String premiacoes) {
-        this.premiacoes = premiacoes;
-    }
-}
